3ed789977487c83bfc6d4390d4931f54917d6679
AlexanderDmitriev <alex9110077@gmail.com>
2022-10-13 17:57:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-class Counter {
-    #number = 10
-  
-    increment() {
-      this.#number++
-    }
-  
-    getNum() {
-      return this.#number
-    }
-  }
-  
-  const counter = new Counter()
-  counter.increment()
-  
-  console.log(counter.number)
+/* const isUnique = (string1) => {
+  return new Set(string1).size===string1.length;
+}
+  console.log(isUnique('abcdef'));
+  console.log(isUnique('1234567'));
+  console.log(isUnique('abcABC'));
+  console.log(isUnique('abcabc')); */
+
+////////////////////////////////////////////////////////////
+
+/*   const flutten = (array) => {
+
+  };
+  console.log(flutten([[1],[[2,3]],[[[4]]]])); */
